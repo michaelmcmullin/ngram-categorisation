39ce214606b628fdb70f3cde8636f31182fbf9c7
mrmcnerd <blueark@outlook.com>
2017-07-31 07:32:52 +0100

    Adds interface for handling sets of N-Grams

---
+++ NGramCategorisation/INGramSet.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace NGramCategorisation
+{
+    /// <summary>
+    /// Represents a set of N-Grams, including methods for parsing a source
+    /// object into multiple N-Grams, and comparing this set with another set
+    /// to determine similarity.
+    /// </summary>
+    /// <typeparam name="T">The type of element in the NGramSet.</typeparam>
+    public interface INGramSet<T> where T : IEquatable<T>
+    {
+        /// <summary>
+        /// Converts a source item into a set of NGram&lt;T&gt; objects.
+        /// </summary>
+        /// <param name="source">The source item to convert to N-Grams.</param>
+        void Parse(T source);
+
+        /// <summary>
+        /// Calculate the distance of a specific NGram&lt;T&gt; object from its
+        /// occurance in this set with its occurance in another set. Lower
+        /// values suggest greater similarity.
+        /// </summary>
+        /// <param name="ngram">The NGram&lt;T&gt; object to compare.</param>
+        /// <param name="otherSet">The INGramSet&lt;T&gt; to compare the
+        /// position with.</param>
+        /// <returns>A value indicating ngram's relative position between this
+        /// set and otherSet. Lower values suggest greater similarity, and
+        /// results are always positive.</returns>
+        double GetDistance(T ngram, INGramSet<T> otherSet);
+
+        /// <summary>
+        /// Calculate the overall distance between this set and otherSet. Lower
+        /// values indicate a greater similarity between both sets.
+        /// </summary>
+        /// <param name="otherSet">The INGramSet&lt;T&gt; to compare with this
+        /// instance.</param>
+        /// <returns>A positive value representing the distance between the two
+        /// sets. Lower values mean greater similarity.</returns>
+        double GetDistance(INGramSet<T> otherSet);
+    }
+}
---
+++ NGramCategorisation/NGramSets/StringSet.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace NGramCategorisation.NGramSets
+{
+    /// <summary>
+    /// Represents a set of string N-Grams, including methods for parsing a
+    /// source object into multiple N-Grams, and comparing this set with another
+    /// set to determine similarity.
+    /// </summary>
+    public class StringSet : INGramSet<string>
+    {
+        protected List<NGram<string>> ngrams = new List<NGram<string>>();
+
+        public double GetDistance(string ngram, INGramSet<string> otherSet)
+        {
+            throw new NotImplementedException();
+        }
+
+        public double GetDistance(INGramSet<string> otherSet)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Parse(string source)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
